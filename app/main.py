d6a9f97856aacf54e32f8e20f320f7109d128376
Miguel <miguelmateo1985@gmail.com>
2026-01-21 13:54:21 +0100

    quito credenciales de base de datos

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from app.api.auth import auth_bp
 from app.api.fields import field_bp
+from app.api.plant import plant_bp
 
 app = Flask(__name__)
 CORS(app)  # ← ESTO HACE LA MAGIA ✨
@@ -9,6 +10,7 @@ CORS(app)  # ← ESTO HACE LA MAGIA ✨
 # Registrar blueprint
 app.register_blueprint(auth_bp)
 app.register_blueprint(field_bp)
+app.register_blueprint(plant_bp)
 
 @app.route('/')
 def home():
